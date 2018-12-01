0437ed8581a297bc41c77ed12c7942bd3767103b
Manas Mishra <manas1.mishra@ril.com>
2018-12-01 11:15:48 +0530

    changes in dockview for input element

--- DrawerVC/DockUsingView/BaseViewController.swift
+++ DrawerVC/DockUsingView/BaseViewController.swift
@@ -38,24 +38,12 @@ class BaseViewController: UIViewController {
     }
     
     @objc func handlePanGesture(panGesture: UIPanGestureRecognizer) {
+        guard let dView = panGesture.view as? DockingView else {return}
         // get translation
         let translation = panGesture.translation(in: view)
-       // print(translation)
+        // print(translation)
         let panX = abs(translation.x)
         let panY = abs(translation.y)
-        let widthMultiplier = 1 - panX/DockingView.DeviceSpecific.width
-        let heightMultiplier = 1 - panY/DockingView.DeviceSpecific.height
-        let newWidthOfDockingView = DockingView.DeviceSpecific.width*widthMultiplier
-        let newHeightOfDockingView = DockingView.DeviceSpecific.height*heightMultiplier
-
-        
-        if let view = panGesture.view {
-            let newFrame = CGRect(x: translation.x, y: translation.y, width: newWidthOfDockingView, height: newHeightOfDockingView)
-            view.frame = newFrame
-        }
-       
-        
-        
         
         switch panGesture.state {
         case .began:
@@ -64,9 +52,30 @@ class BaseViewController: UIViewController {
         case .changed:
             // add something you want to happen when the Label Panning has been change ( during the moving/panning )
             print("In changed case")
+            dView.dockingViewState = .transition
+            let newSize = dView.sizeOfDockingView(panX: panX, panY: panY)
+            let newFrame = CGRect(x: translation.x, y: translation.y, width: newSize.width, height: newSize.height)
+            dView.frame = newFrame
         case .ended:
             // add something you want to happen when the Label Panning has ended
             print("In ended case")
+            let newSize = dView.sizeOfDockingView(panX: panX, panY: panY)
+            
+            if ((newSize.width < dView.thresholdSize.width) && ((newSize.height < dView.thresholdSize.height))) {
+                dView.dockingViewState = .docked
+                let newSize = dView.sizeOfDockingView(panX: panX, panY: panY)
+                UIView.animate(withDuration: 0.3) {
+                    let newFrame = CGRect(x: DockingView.DeviceSpecific.width*3/4, y: DockingView.DeviceSpecific.height*3/4, width: DockingView.DeviceSpecific.width/4, height: DockingView.DeviceSpecific.height/4)
+                    dView.frame = newFrame
+                }
+            } else {
+                dView.dockingViewState = .expanded
+                let newSize = dView.sizeOfDockingView(panX: panX, panY: panY)
+                UIView.animate(withDuration: 0.3) {
+                    let newFrame = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+                    dView.frame = newFrame
+                }
+            }
         default:
             print("In default case")
         }
--- DrawerVC/DockUsingView/DockingView.swift
+++ DrawerVC/DockUsingView/DockingView.swift
@@ -14,6 +14,7 @@ class DockingView: UIView {
         static let width = UIScreen.main.bounds.width
     }
 
+    @IBOutlet weak var topViewRatioConstarint: NSLayoutConstraint!
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
@@ -22,11 +23,46 @@ class DockingView: UIView {
     }
     */
     @IBOutlet weak var topView: UIView!
+    var dockedStateWidthWRTDeviceWidth: CGFloat!
     var dockingViewState: DockingViewState = .dismissed
+    var tvRatio: CGFloat!
+    var thresholdHeightForTransitionWRTScreenHegiht: CGFloat!
     
-    class func initialize(_ frame: CGRect) -> DockingView {
+    var thresholdSize: CGSize {
+        return CGSize(width: DeviceSpecific.width/thresholdHeightForTransitionWRTScreenHegiht, height: DeviceSpecific.height/thresholdHeightForTransitionWRTScreenHegiht)
+    }
+    
+    func sizeOfDockingView(panX: CGFloat, panY: CGFloat) -> CGSize {
+        switch dockingViewState {
+        case .expanded:
+            return CGSize(width: DeviceSpecific.width, height: DeviceSpecific.height)
+        case .docked:
+            let newWidth = DeviceSpecific.width/dockedStateWidthWRTDeviceWidth
+            let newHeight = newWidth*(1/tvRatio)
+            return CGSize(width: newWidth, height: newHeight)
+        case .dismissed:
+            break
+        case .transition:
+            let widthMultiplier = 1 - panX/DockingView.DeviceSpecific.width
+            let heightMultiplier = 1 - panY/DockingView.DeviceSpecific.height
+            let newWidthOfDockingView = DockingView.DeviceSpecific.width*widthMultiplier
+            let newHeightOfDockingView = DockingView.DeviceSpecific.height*heightMultiplier
+            return CGSize(width: newWidthOfDockingView, height: newHeightOfDockingView)
+            
+        }
+        return CGSize.zero
+    }
+    
+    func <#name#>(<#parameters#>) -> <#return type#> {
+        <#function body#>
+    }
+    
+    class func initialize(_ frame: CGRect, topViewPropertion tVRatio: CGFloat = 16/9, dockedStateRatio: CGFloat = 2.5, thresholdHeight: CGFloat = 0.5) -> DockingView {
         let dView = Bundle.main.loadNibNamed("DockingView", owner: self, options: nil)?.first as! DockingView
         dView.frame = frame
+        dView.dockedStateWidthWRTDeviceWidth = dockedStateRatio
+        dView.topViewRatioConstarint.constant = tVRatio
+        dView.thresholdHeightForTransitionWRTScreenHegiht = 0.5
         return dView
     }
     
